1cf93fb97cb7648d5560efe378028ba9149ebf85
Daniel <danielpunk4@gmail.com>
2023-03-31 15:53:43 -0500

    redes

--- wp-content/themes/dunapower/footer.php
+++ wp-content/themes/dunapower/footer.php
@@ -31,10 +31,10 @@
                     <!-- socials start-->
                     <nav class="socials-holder">
                         <ul class="socials-primary">
-                            <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                            <!-- <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
                             <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
                             <li><a href="#"><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
-                            <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                            <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li> -->
                         </ul>
                     </nav>
                     <!-- socials end -->
